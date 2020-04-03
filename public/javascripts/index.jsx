bd4baa9dc481f28f01bf8b926bc6ba58bf9a9c8e
Yiğitcan Uçan <ucanyiit@gmail.com>
2020-04-03 12:49:01 +0300

    Changes on listing items

--- public/javascripts/index.jsx
+++ public/javascripts/index.jsx
@@ -5,26 +5,46 @@ class Restaurant extends React.Component {
     let id = item.match(idPattern) - 1;
     let items = window.data.restaurant.active_menu.menu.items;
     let data = items[id].item;
+    let img;
+    if (data.images.length > 0){
+      img = data.images[0]["100"];
+    }
+    else img = "";
     return (
-      <a href="#" class="list-group-item list-group-item-action">
-        <div class="d-flex bd-highlight">
-          <div class="flex-grow-1">
-            <h5>{data["name"]}</h5>
+      <div class="card mb-2 mt-2">
+        <div class="d-flex">
+          <div class="flex-shrink-1 pl-3 pt-3">
+            <img class="rounded" src={img}/>
           </div>
-          <div>
-            <small>{data["price"]}</small>
+          <div class="flex-grow-1">
+            <div class="card-body">
+              <div class="d-flex">
+                <div class="flex-grow-1">
+                  <h4>{data["name"]}</h4>
+                </div>
+                <div>
+                  <p class="text-success font-weight-bold text-monospace">{data["price"]} TL</p>
+                </div>
+              </div>
+              <div class="d-flex">
+                <div>
+                  <small class="mb-1">{data["ingredients"].join(", ")}</small>
+                </div>
+                <div class="ml-auto p-1">
+                  <button type="button" class="btn btn-outline-secondary">Favorilere ekle</button>
+                </div>
+              </div>
+            </div>
           </div>
         </div>
-        <p class="mb-1">Donec id elit non mi porta gravida at eget metus. Maecenas sed diam eget risus varius blandit.</p>
-        <small>Donec id elit non mi porta.</small>
-      </a>
+      </div>
     )
   }
 
   printSections(item) {
     return (
       <div>
-        <h3>{item.section}</h3>
+        <h2>{item.section}</h2>
         <div class="list-group">
           {item.items.map(i => (this.printItem(i)))}
         </div>
