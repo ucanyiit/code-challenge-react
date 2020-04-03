822852f449d07c7280fa6e9179452a4920ec7ef7
Yiğitcan Uçan <ucanyiit@gmail.com>
2020-04-03 10:42:05 +0300

    Added a list of items/sections in the main page

--- public/javascripts/index.jsx
+++ public/javascripts/index.jsx
@@ -1,21 +1,44 @@
 class Restaurant extends React.Component {
+
+  printItem(item){
+    let idPattern = /\d+/g;
+    let id = item.match(idPattern)-1;
+    let items = window.data.restaurant.active_menu.menu.items;
+    return (
+      <li>
+        {items[id].item["name"]}
+      </li>
+    )
+  }
+
+  printSections(item) {
+    return (
+      <div>
+        <h3>{item.section}</h3>
+        <ul>  
+          {item.items.map(i => (this.printItem(i)))}
+        </ul>
+      </div>
+    )
+}
+
+  getItemLists() {
+    let sections = window.data.restaurant.active_menu.menu.item_order;
+
+    return sections.map(section => (this.printSections(section)))
+  }
+
   render() {
     const restaurant = window.data.restaurant;
-    return <div>
-    <h1>Welcome to {restaurant.name}</h1>
-    <div>
-      <h2>
-        Please find our menu below
-      </h2>
-    </div>
-    </div>
+    console.log(window.data);
+    return this.getItemLists()
   }
 }
 
 
-$(function() {
+$(function () {
   ReactDOM.render(
-    <Restaurant/>,
+    <Restaurant />,
     document.getElementById('react-root')
   );
 
