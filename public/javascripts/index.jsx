8c1603570f3ed19fd194d46bbbe3e6397b2e3e69
Yiğitcan Uçan <ucanyiit@gmail.com>
2020-04-03 11:54:24 +0300

    Bootstrap version ugrade (and some changes on list)

--- public/javascripts/index.jsx
+++ public/javascripts/index.jsx
@@ -1,13 +1,23 @@
 class Restaurant extends React.Component {
 
-  printItem(item){
+  printItem(item) {
     let idPattern = /\d+/g;
-    let id = item.match(idPattern)-1;
+    let id = item.match(idPattern) - 1;
     let items = window.data.restaurant.active_menu.menu.items;
+    let data = items[id].item;
     return (
-      <li>
-        {items[id].item["name"]}
-      </li>
+      <a href="#" class="list-group-item list-group-item-action">
+        <div class="d-flex bd-highlight">
+          <div class="flex-grow-1">
+            <h5>{data["name"]}</h5>
+          </div>
+          <div>
+            <small>{data["price"]}</small>
+          </div>
+        </div>
+        <p class="mb-1">Donec id elit non mi porta gravida at eget metus. Maecenas sed diam eget risus varius blandit.</p>
+        <small>Donec id elit non mi porta.</small>
+      </a>
     )
   }
 
@@ -15,12 +25,12 @@ class Restaurant extends React.Component {
     return (
       <div>
         <h3>{item.section}</h3>
-        <ul>  
+        <div class="list-group">
           {item.items.map(i => (this.printItem(i)))}
-        </ul>
+        </div>
       </div>
     )
-}
+  }
 
   getItemLists() {
     let sections = window.data.restaurant.active_menu.menu.item_order;
